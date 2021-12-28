56ae4786d7350aa56b0bdfecc33ed2403abda1da
nora galabe <nchang.najeh@gmail.com>
2021-12-27 20:13:04 -0500

    added question of day file

---
+++ question_day.py
@@ -0,0 +1,15 @@
+1. Greet = "Good morning folks"
+      print(Greet)
+
+2. You can use a harsh mark (#) on a single line or three doubl quotes(""") open and close
+              for a block or multi-lines
+              
+3. Create a variable with the numeric value by declaring the number and passing it into a value
+                 x = 7
+                print(x)
+
+4. Extension for python files is PY
+
+5. x = 8.6
+
+
