47f8ef5aeb2fb57dc48ba3e422dff84263f5cc75
nora galabe <nchang.najeh@gmail.com>
2021-12-30 16:55:12 -0500

    added today's answers

--- question_day.py
+++ question_day.py
@@ -1,18 +1,76 @@
- 1. print(type([])is list)
+#Wed-29th
+
+1. for num in range(2,-5,-1):
+              print(num, end=", ")
+2, 1, 0, -1, -2, -3, -4,
+
+2.  if a > 0:
+    if b < 0:
+       x = x + 5
+  elif a > 5:
+       x = x + 4
+  else: 
+      x = x + 3
+  else:
+      x = x + 2
+      print(x) 
+
+
+3. for num in range(10, 14):
+       for i in range(2, num):
+       if num%i == 1:
+          print(num)
+         break
+   10
+   11
+   12
+   13
+
+4. if a + b:
+      print('True')
+   else:
+      print('False')
 True
 
-2. [5]: def myfun():
-       x  = x + 1
-        print(x)
-        myfunc()
-        print(x)
-        x = 75
 
-3. print(bool(0), bool(3.14159), bool(-3), bool(1.0 + 1j))
-False True True True
+5.var = 10
+      for i in range(10):
+       for j in range(2,10,1):
+        if var % 2 == 0:
+         conitinue
+         var += 1
+          var += 1
+     else:
+        var += 1
+        print(var)
+*********************************************************************************
 
-4.type(range(5))
-range
+#Thursd-30th:
 
-5. print(type(10))
-<class 'int'>
+# What is the output of print(2 ** 3 ** 2)
+
+512 
+
+#What is the output of: 
+print(10 - 4 * 2)
+
+2
+
+# What is the value of the following Python Expression
+print(36 / 4)
+
+9.0
+
+# What is the output of the following Python code
+x = 10
+y = 50
+if x ** 2 > 100 and y < 100:
+        print(x, y)
+?
+
+# What is the output of the following code
+        x = 100
+        y = 50
+        print(x and y)
+
+50
