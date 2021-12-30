4a8fd8f04806d11a4137b393ed323584d4aba9cd
nora galabe <nchang.najeh@gmail.com>
2021-12-29 20:23:19 -0500

    I added  file

---
+++ quest_day_29.py
@@ -0,0 +1,45 @@
+1. for num in range(2,-5,-1):
+              print(num, end=", ")
+2, 1, 0, -1, -2, -3, -4,
+
+2.  if a > 0:
+    if b < 0:
+       x = x + 5
+  elif a > 5:
+       x = x + 4
+  else: 
+      x = x + 3
+  else:
+      x = x + 2
+      print(x) 
+
+
+3. for num in range(10, 14):
+       for i in range(2, num):
+       if num%i == 1:
+          print(num)
+         break
+   10
+   11
+   12
+   13
+
+4. if a + b:
+      print('True')
+   else:
+      print('False')
+True
+
+
+5.var = 10
+      for i in range(10):
+       for j in range(2,10,1):
+        if var % 2 == 0:
+         continue
+         var += 1
+          var += 1
+     else:
+        var += 1
+        print(var)
+
+                         
